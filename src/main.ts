261cf28bf71aefaadff70fd182a083792e543663
纪宇航 <jiyuhang@jctrans.net>
2023-11-11 22:04:07 +0800

    feat: init swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,28 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+
+  // 设置全局前缀
+  app.setGlobalPrefix('/v1/api');
+
+  const config = new DocumentBuilder()
+    .setTitle('API Example')
+    .setDescription('The v1 API description')
+    .setVersion('1.0')
+    .setBasePath('/v1/api')
+    .addBasicAuth()
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('/', app, document);
+
   await app.listen(3000);
+  console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
