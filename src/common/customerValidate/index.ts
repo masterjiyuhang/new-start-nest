8f358fb16500f3272c9fdab63fd81d8242ce8e92
纪宇航 <jiyuhang@jctrans.net>
2023-11-23 15:23:16 +0800

    fix: 修改自定义邮箱校验

--- src/common/customerValidate/index.ts
+++ src/common/customerValidate/index.ts
@@ -7,21 +7,35 @@ import {
 
 @ValidatorConstraint({ name: 'isNotBlacklistedDomain', async: false })
 export class IsNotBlacklistedDomain implements ValidatorConstraintInterface {
+  // 在类外部定义 blacklistedDomains，以便在 defaultMessage 方法中访问
+  private static blacklistedDomains: Record<string, string> = {
+    'qq.com': '不允许使用 QQ 邮箱地址。',
+    'example.com': '不允许使用示例域名。',
+    // 根据需要添加更多的域名和对应的错误消息
+  };
+
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   validate(email: string, _args: ValidationArguments) {
-    const blacklistedDomains: Record<string, string> = {
-      'qq.com': 'QQ email addresses are not allowed.',
-      'example.com': 'Example domain is not allowed.',
-      // 可以根据需要添加更多的域名和对应的错误消息
-    };
-
     const [, domain] = email.split('@');
 
-    return !(domain in blacklistedDomains);
+    if (domain in IsNotBlacklistedDomain.blacklistedDomains) {
+      return false;
+    }
+
+    return true;
   }
 
   defaultMessage(args: ValidationArguments) {
-    return `Email address is not allowed: ${args.value}`;
+    const email = args.value as string;
+    const [, domain] = email.split('@');
+
+    // 检查黑名单中是否有特定的错误消息
+    if (domain in IsNotBlacklistedDomain.blacklistedDomains) {
+      return IsNotBlacklistedDomain.blacklistedDomains[domain];
+    }
+
+    // 如果域名不在黑名单中，则返回默认错误消息
+    return `不允许使用该邮箱地址: ${email}`;
   }
 }
 
