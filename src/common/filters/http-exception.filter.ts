502f8fc61b38c2ed4398141a2d3c7f9f521bdb86
纪宇航 <jiyuhang@jctrans.net>
2024-07-23 11:03:35 +0800

    feat: 日期时区偏移量处理

--- src/common/filters/http-exception.filter.ts
+++ src/common/filters/http-exception.filter.ts
@@ -25,10 +25,28 @@ export class HttpExceptionFilter implements ExceptionFilter {
       });
       return;
     }
+    // 获取当前日期时间
+    const currentDate = new Date();
 
+    // 获取当前时间戳（以毫秒为单位）
+    const currentTime = currentDate.getTime();
+
+    // 增加8小时（8小时 * 60分钟 * 60秒 * 1000毫秒）
+    const adjustedTime = currentTime + 8 * 60 * 60 * 1000;
+
+    // 创建一个新的Date对象，使用调整后的时间戳
+    const adjustedDate = new Date(adjustedTime);
+
+    // 转换为ISO字符串格式
+    const isoString = adjustedDate.toISOString();
+
+    console.log(
+      '🚀 ~ file: http-exception.filter.ts:41 ~ HttpExceptionFilter ~ isoString:',
+      isoString,
+    );
     response.status(status).json({
       code: status,
-      timestamp: new Date().toISOString(),
+      timestamp: isoString,
       url: request.url,
       describe: (exception as any)?.response?.message || exception.message,
     });
