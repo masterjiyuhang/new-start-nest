4f6d3b7ba3739eb822be1ed9764e2db93c88fedc
纪宇航 <jiyuhang@jctrans.net>
2025-01-21 14:50:54 +0800

    chore: add pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  apps: [
+    {
+      name: 'nest-server-app',
+      script: 'dist/main.js',
+      // env: {
+      //   NODE_ENV: 'development',
+      //   PORT: 3000,
+      //   DATABASE_HOST: 'localhost',
+      //   DATABASE_PORT: 5432,
+      //   DATABASE_USER: 'username',
+      //   DATABASE_PASSWORD: 'password',
+      // },
+      env_prod: {
+        NODE_ENV: 'prod',
+        PORT: 3234,
+        DATABASE_HOST: 'config.erhang.fun',
+        DATABASE_PORT: 3306,
+        DATABASE_USER: 'root',
+        DATABASE_PASSWORD: 'cd13f6e448fb9d31',
+        DATABASE_NAME: 'management_test',
+      },
+    },
+  ],
+};
